ecfcbdeaf1fb563d33aa8898cf5d6652cb2e774c
aaronloh16 <aaronloh04@gmail.com>
2023-07-12 11:14:25 -0400

    Added -h/--help option to CLI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import ca.mcscert.jpipe.model.Justification;
 import ca.mcscert.jpipe.model.Unit;
 
 import org.apache.commons.cli.*;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -18,45 +19,52 @@ public class Main {
 
     private static CommandLine setupCLI(String[] args) {
         Options options = new Options();
-
         Option input = new Option("i", "input", true, "input file path");
         input.setRequired(true);
         options.addOption(input);
-    
+
         Option output = new Option("o", "output", true, "output file path");
         output.setRequired(true);
         options.addOption(output);
-    
+
         Option diagram = new Option("d", "diagram", true, "diagram name");
         diagram.setRequired(true);
         options.addOption(diagram);
-        
+
+        Option help = new Option("h", "help", false, "display help message");
+        options.addOption(help);
+
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd;
-    
+
         try {
             cmd = parser.parse(options, args);
+            if (cmd.hasOption("h")) {
+                formatter.printHelp("jpipe", options);
+                System.exit(0);
+            }
         } catch (ParseException e) {
             System.out.println(e.getMessage());
             formatter.printHelp("jpipe", options);
             throw new RuntimeException("Error parsing command line arguments", e);
         }
-    
+
         return cmd;
     }
-    
+
     public static void main(String[] args) {
         CommandLine cmd = setupCLI(args);
         String inputFile = cmd.getOptionValue("input");
         String outputFile = cmd.getOptionValue("output");
         String diagramName = cmd.getOptionValue("diagram");
-        
+
         Compiler compiler = new Compiler();
         Unit unit;
+
         try {
             unit = compiler.compile(inputFile);
-            
+
             boolean diagramExists = false;
             for (Justification j : unit.getJustificationSet()) {
                 if (j.getName().equals(diagramName)) {
@@ -64,31 +72,32 @@ public class Main {
                     break;
                 }
             }
-        
+
             if (!diagramExists) {
                 throw new IllegalArgumentException(ANSI_RED + "Diagram not found: " + diagramName + ANSI_RESET);
             }
-        
+
             File outputDirectory = new File(outputFile).getParentFile();
             if (!outputDirectory.exists()) {
                 throw new IllegalArgumentException(ANSI_RED + "Output directory does not exist: " + outputDirectory.getPath() + ANSI_RESET);
             }
-        
+
             Exportation<Justification> exporter = new DiagramExporter();
-            for(Justification j: unit.getJustificationSet()) {
+            for (Justification j : unit.getJustificationSet()) {
                 if (j.getName().equals(diagramName)) {
                     String outputFilePath = removeFileExtension(outputFile) + "_" + j.getName() + ".png";
                     exporter.export(j, outputFilePath);
-                }    
+                }
             }
-        } catch (FileNotFoundException e) {
-            System.err.println(ANSI_RED + "File not found: " + e.getMessage() + ANSI_RESET);
-        } catch (CompilationError | TypeError | ExportationError e) {
-            System.err.println(ANSI_RED + e.getMessage() + ANSI_RESET);
+        } catch (FileNotFoundException fnfe) {
+            throw new RuntimeException(ANSI_RED + "File not found: " + fnfe.getMessage() + ANSI_RESET, fnfe);
+        } catch (CompilationError | TypeError | ExportationError err) {
+            throw new RuntimeException(ANSI_RED + err.getMessage() + ANSI_RESET, err);
         }
     }
-    
+
     private static String removeFileExtension(String filename) {
+        // https://www.baeldung.com/java-filename-without-extension
         return filename.replaceAll("(?<!^)[.][^.]*$", "");
     }
 }
