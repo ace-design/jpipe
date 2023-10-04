10a88925db1460a2497cccaa268f0a96edcffec9
Sébastien Mosser <sebastien.mosser@gmail.com>
2023-10-04 13:28:02 +0200

    fix checkstyle / JustificationDiagram

--- src/main/java/ca/mcscert/jpipe/model/JustificationDiagram.java
+++ src/main/java/ca/mcscert/jpipe/model/JustificationDiagram.java
@@ -2,16 +2,21 @@ package ca.mcscert.jpipe.model;
 
 import ca.mcscert.jpipe.model.justification.Conclusion;
 
+/**
+ * Interface to model commonality of a Justification diagram (pattern or concrete).
+ */
 public interface JustificationDiagram extends Visitable {
 
     /**
-     * Get the name of the Justification Diagram
+     * Get the name of the Justification Diagram.
+     *
      * @return the unique name, as a string
      */
     String name();
 
     /**
-     * Give access to the entry point (the conclusion) of the diagram
+     * Give access to the entry point (the conclusion) of the diagram.
+     *
      * @return the diagram's conclusion
      */
     Conclusion conclusion();
