513c4834d157d4e5af9287948263554e67f1fcfb
Sébastien Mosser <sebastien.mosser@gmail.com>
2023-10-04 13:50:46 +0200

    fix checkstyle / SubConclusion

--- src/main/java/ca/mcscert/jpipe/model/justification/SubConclusion.java
+++ src/main/java/ca/mcscert/jpipe/model/justification/SubConclusion.java
@@ -2,22 +2,35 @@ package ca.mcscert.jpipe.model.justification;
 
 import ca.mcscert.jpipe.model.Visitable;
 import ca.mcscert.jpipe.visitors.AbstractVisitor;
-
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Represent the SubConclusion AST node in the model.
+ */
 public class SubConclusion extends Support implements Visitable {
 
-    private Set<Strategy> supports = new HashSet<>();
+    // supporting strategies leading to that sub conclusion.
+    private final Set<Strategy> supports = new HashSet<>();
 
     public SubConclusion(String identifier, String label) {
         super(identifier, label);
     }
 
+    /**
+     * add a supporting strategy that helps in reaching this sub-conclusion.
+     *
+     * @param strategy the instance of strategy supporting the sub conclusion
+     */
     public void addSupport(Strategy strategy) {
         this.supports.add(strategy);
     }
 
+    /**
+     * Get the set of strategies that support the sub-conclusion.
+     *
+     * @return the aforementioned set
+     */
     public Set<Strategy> getSupports() {
         return supports;
     }
