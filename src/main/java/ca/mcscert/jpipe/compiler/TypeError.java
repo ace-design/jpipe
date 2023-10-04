0434dd0b42f0ec96a021283e36d5dac87ee0f312
Sébastien Mosser <sebastien.mosser@gmail.com>
2023-10-04 09:20:57 +0200

    fix checkstyle / TypeError

--- src/main/java/ca/mcscert/jpipe/compiler/TypeError.java
+++ src/main/java/ca/mcscert/jpipe/compiler/TypeError.java
@@ -1,5 +1,8 @@
 package ca.mcscert.jpipe.compiler;
 
+/**
+ * Custom class to identify typing error at runtime while compiling.
+ */
 public class TypeError extends RuntimeException {
 
     public TypeError(String message) {
