382298ea02190380a084ed39940bf3d407f490b3
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2023-11-18 11:46:59 -0500

    [Format] Formatted code for CheckStyle
    Reformatted code to align with CheckStyle.

--- src/main/java/ca/mcscert/jpipe/model/justification/Conclusion.java
+++ src/main/java/ca/mcscert/jpipe/model/justification/Conclusion.java
@@ -10,40 +10,40 @@ import java.util.Set;
  */
 public class Conclusion extends JustificationElement implements Visitable, Cloneable {
 
-    // The set of supporting strategies.
-    private Set<Strategy> supports = new HashSet<>();
+  // The set of supporting strategies.
+  private Set<Strategy> supports = new HashSet<>();
 
-    public Conclusion(String identifier, String label) {
-        super(identifier, label);
-    }
+  public Conclusion(String identifier, String label) {
+    super(identifier, label);
+  }
 
-    /**
-     * Add a supporting strategy to that conclusion.
-     *
-     * @param from the strategy supporting the conclusion.
-     */
-    public void addSupport(Strategy from) {
-        this.supports.add(from);
-    }
+  /**
+   * Add a supporting strategy to that conclusion.
+   *
+   * @param from the strategy supporting the conclusion.
+   */
+  public void addSupport(Strategy from) {
+    this.supports.add(from);
+  }
 
-    /**
-     * Return the set of supporting strategies for that conclusion.
-     *
-     * @return the aforementioned set.
-     */
-    public Set<Strategy> getSupports() {
-        return supports;
-    }
+  /**
+   * Return the set of supporting strategies for that conclusion.
+   *
+   * @return the aforementioned set.
+   */
+  public Set<Strategy> getSupports() {
+    return supports;
+  }
 
-    @Override
-    public void accept(AbstractVisitor<?> visitor) {
-        visitor.visit(this);
-    }
+  @Override
+  public void accept(AbstractVisitor<?> visitor) {
+    visitor.visit(this);
+  }
 
-    @Override
-    public Conclusion clone() throws CloneNotSupportedException {
-        Conclusion cloned_conclusion = (Conclusion) super.clone();
-        cloned_conclusion.supports = new HashSet<>();
-        return cloned_conclusion;
-    }
+  @Override
+  public Conclusion clone() throws CloneNotSupportedException {
+    Conclusion clonedConclusion = (Conclusion) super.clone();
+    clonedConclusion.supports = new HashSet<>();
+    return clonedConclusion;
+  }
 }
--- src/main/java/ca/mcscert/jpipe/model/justification/SubConclusion.java
+++ src/main/java/ca/mcscert/jpipe/model/justification/SubConclusion.java
@@ -10,40 +10,40 @@ import java.util.Set;
  */
 public class SubConclusion extends Support implements Visitable, Cloneable {
 
-    // supporting strategies leading to that sub conclusion.
-    private Set<Strategy> supports = new HashSet<>();
+  // supporting strategies leading to that sub conclusion.
+  private Set<Strategy> supports = new HashSet<>();
 
-    public SubConclusion(String identifier, String label) {
-        super(identifier, label);
-    }
+  public SubConclusion(String identifier, String label) {
+    super(identifier, label);
+  }
 
-    /**
-     * add a supporting strategy that helps in reaching this sub-conclusion.
-     *
-     * @param strategy the instance of strategy supporting the sub conclusion
-     */
-    public void addSupport(Strategy strategy) {
-        this.supports.add(strategy);
-    }
+  /**
+   * add a supporting strategy that helps in reaching this sub-conclusion.
+   *
+   * @param strategy the instance of strategy supporting the sub conclusion
+   */
+  public void addSupport(Strategy strategy) {
+    this.supports.add(strategy);
+  }
 
-    /**
-     * Get the set of strategies that support the sub-conclusion.
-     *
-     * @return the aforementioned set
-     */
-    public Set<Strategy> getSupports() {
-        return supports;
-    }
+  /**
+   * Get the set of strategies that support the sub-conclusion.
+   *
+   * @return the aforementioned set
+   */
+  public Set<Strategy> getSupports() {
+    return supports;
+  }
 
-    @Override
-    public void accept(AbstractVisitor<?> visitor) {
-        visitor.visit(this);
-    }
+  @Override
+  public void accept(AbstractVisitor<?> visitor) {
+    visitor.visit(this);
+  }
 
-    @Override
-    public SubConclusion clone() throws CloneNotSupportedException {
-        SubConclusion cloned_subconclusion = (SubConclusion) super.clone();
-        cloned_subconclusion.supports = new HashSet<>();
-        return cloned_subconclusion;
-    }
+  @Override
+  public SubConclusion clone() throws CloneNotSupportedException {
+    SubConclusion clonedSubConclusion = (SubConclusion) super.clone();
+    clonedSubConclusion.supports = new HashSet<>();
+    return clonedSubConclusion;
+  }
 }
