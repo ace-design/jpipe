047142a4b6e7b5ac4b9ad66ea6bab3efeb8c9d22
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2024-03-23 20:36:05 -0400

    [Feature] Configured code to use LSP server
    Modified extension to now use Alex's LSP server.

--- jpipe-ext/out/extension.js
+++ jpipe-ext/out/extension.js
@@ -2,13 +2,48 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const editorReader_1 = require("./editorReader");
+const vscode_1 = require("vscode");
+const node_1 = require("vscode-languageclient/node");
+let client;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-function activate(context) {
+async function activate(context) {
     context.subscriptions.push(editorReader_1.editorReader.register(context));
+    const lspConfig = vscode_1.workspace.getConfiguration("noname-jpipe.language_server", null);
+    let bin_path = lspConfig.get("path", null);
+    // let bin_path = "/Users/nirmalchaudhari/Desktop/lever-jpipe/language-server-jpipe/target/release/jpipe-language-server";
+    if (!bin_path) {
+        vscode_1.window.showInformationMessage(`No language server path specified.`);
+        bin_path = "./jpipe-language-server";
+        await lspConfig.update("path", bin_path, true);
+    }
+    const serverOptions = {
+        command: bin_path,
+    };
+    // Options to control the language client
+    const clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'jpipe' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+    // Create the language client and start the client.
+    client = new node_1.LanguageClient('language-server-jpipe', 'jpipe Language Server', serverOptions, clientOptions);
+    // Start the client. This will also launch the server
+    return client.start().catch(reason => {
+        vscode_1.window.showWarningMessage(`Failed to run language server: ${reason}`);
+        client = null;
+    });
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- jpipe-ext/src/extension.ts
+++ jpipe-ext/src/extension.ts
@@ -3,11 +3,66 @@
 import * as vscode from 'vscode';
 import { editorReader } from './editorReader';
 
+import { workspace, ExtensionContext, window, ProgressLocation } from 'vscode';
+import * as fs from "fs";
+
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient | null;
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(editorReader.register(context));
+
+	const lspConfig = workspace.getConfiguration("noname-jpipe.language_server", null);
+    let bin_path = lspConfig.get<string | null>("path", null);
+    // let bin_path = "/Users/nirmalchaudhari/Desktop/lever-jpipe/language-server-jpipe/target/release/jpipe-language-server";
+
+    if (!bin_path) {
+        window.showInformationMessage(`No language server path specified.`);
+        bin_path = "./jpipe-language-server";
+        await lspConfig.update("path", bin_path, true);
+    }
+
+    const serverOptions: ServerOptions = {
+        command: bin_path,
+    };
+
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'jpipe' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'language-server-jpipe',
+        'jpipe Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    // Start the client. This will also launch the server
+    return client.start().catch(reason => {
+        window.showWarningMessage(`Failed to run language server: ${reason}`);
+        client = null;
+    });
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
