cda2164cfbf23717918d99a0fe4953253bcdbc15
Sébastien Mosser <sebastien.mosser@gmail.com>
2024-05-16 11:51:11 -0400

    initial skel for action based compiler

---
+++ ab-compiler/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/** Entry point for the compiler.
+ *
+ * @author mosser
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
