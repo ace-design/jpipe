507631d834f8e9c898a50c3e3cf6a74d8137e122
braunc8 <chloecbraun@gmail.com>
2024-05-29 15:25:04 -0400

    returns correct options for first element of support if lowerase

--- langium/Jpipe/src/language/services/jpipe-completion-provider.ts
+++ langium/Jpipe/src/language/services/jpipe-completion-provider.ts
@@ -5,42 +5,36 @@ import { stream } from "../../../node_modules/langium/src/utils/stream.js"
 
 export class JpipeCompletionProvider extends DefaultCompletionProvider{
     
+    //needs refactoring
     protected override getReferenceCandidates(refInfo: ReferenceInfo, _context: CompletionContext): Stream<AstNodeDescription> {
-        
         let typeMap: Map<string, string[]> = new Map<string,string[]>();
         typeMap.set('evidence', ['strategy']);
         typeMap.set('strategy', ['sub-conclusion', 'conclusion']);
         typeMap.set('sub-conclusion', ['strategy', 'conclusion']);
         typeMap.set('conclusion', []);
+        
         let allPotentials = this.scopeProvider.getScope(refInfo).getAllElements();
         let newPotentials:AstNodeDescription[] = [];
-        if(isSupport(_context.node)){
-            _context.node.supporter
-        }
+
         allPotentials.forEach((potential) =>{
             if(isInstruction(potential.node)){
                 let potentialKind = potential.node.kind;
-                
-            if(isSupport(_context.node)){
-                console.log("potential: " + potential.node.name +"\tpotential kind: " + potentialKind + "\tfirst kind: " + _context.node.supporter.ref?.kind);
-                
-                if(_context.node.supporter.ref !== undefined){
-                    let firstKind=_context.node.supporter.ref.kind;                    
-                    let allowableTypes: string[] | undefined = [];
-                    
-                    allowableTypes=typeMap.get(firstKind);
-                    allowableTypes?.forEach((e) => {console.log(e)});
-                    if(allowableTypes?.includes(potentialKind)){
-                        console.log("potential: " + potential.node.name +"\tpotential kind: " + potentialKind);
+                if(isSupport(_context.node)){
+                    if(_context.node.supporter.ref !== undefined){
+                        let firstKind=_context.node.supporter.ref.kind;                    
+                        let allowableTypes: string[] | undefined = [];
+                        
+                        allowableTypes=typeMap.get(firstKind);
+
+                        if(allowableTypes?.includes(potentialKind)){
+                            newPotentials.push(potential);
+                        }                    
+                    }else{
                         newPotentials.push(potential);
-                    }                    
+                    }
                 }
             }
-            }
-            
-
         });
         return stream(newPotentials);
     }
-
 }
