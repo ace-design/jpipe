9aae67204f4422a8d953b0067091bf527daa1ecb
braunc8 <chloecbraun@gmail.com>
2024-05-30 15:22:59 -0400

    Completion works correctly for right and left supporting statement variables in all cases, cleaned code

--- langium/Jpipe/src/language/services/jpipe-completion-provider.ts
+++ langium/Jpipe/src/language/services/jpipe-completion-provider.ts
@@ -4,132 +4,87 @@ import { isSupport, isVariable } from "../generated/ast.js";
 import { stream } from "../../../node_modules/langium/src/utils/stream.js"
 
 export class JpipeCompletionProvider extends DefaultCompletionProvider{
-    typeMap: Map<string, string[]> = new Map<string,string[]>([
+    //track which variable types can support other variable types
+    readonly typeMap: Map<string, string[]> = new Map<string,string[]>([
         ['evidence', ['strategy']],
         ['strategy', ['sub-conclusion', 'conclusion']],
         ['sub-conclusion', ['strategy', 'conclusion']],
         ['conclusion', []] 
     ]);
-    //needs refactoring
+
+    //filters reference candidates for variables in support statements for autocompletion
     protected override getReferenceCandidates(refInfo: ReferenceInfo, _context: CompletionContext): Stream<AstNodeDescription> {
         let allPotentials = this.scopeProvider.getScope(refInfo).getAllElements();
         let newPotentials:AstNodeDescription[] = [];
-        
         let allVariables:AstNodeDescription[] = [];
 
+        //tracking which are variables vs keywords
         allPotentials.forEach((potential) =>{
             if(isVariable(potential.node)){
-                console.log("pushing: " + potential.node.name + " to all variables");
                 allVariables.push(potential);
             }else{
                 newPotentials.push(potential);
             }
         });
 
+        //if the current context is of a supporting statement, determines which variables should appear for autocomplete
         if(isSupport(_context.node)){
-            console.log("context is support... getting types");
             if(_context.node.supporter.ref !== undefined){
-                console.log("getting right element");
-                this.getSupporteeVariables(allVariables,_context).forEach((v)=>{
+                this.getRightVariables(allVariables,_context).forEach((v)=>{
                     newPotentials.push(v);
                 });
             }else{
-                /*
-                console.log("getting left element");
-                this.getSupporterVariables(allVariables,_context).forEach((v)=>{
+                this.getLeftVariables(allVariables,_context).forEach((v)=>{
                     newPotentials.push(v);
                 });
-                */
             }
         }
         return stream(newPotentials);
     }
-
-    getSupporteeVariables(allVariables: AstNodeDescription[], _context: CompletionContext): AstNodeDescription[]{
-        let supporteeVariables: AstNodeDescription[] = [];
+    
+    //autocompletes right-side variables so that only those which fit the format are shown
+    //ex. if your JD defines evidence 'e1', strategy 'e2' and conclusion e3', when starting the statement:
+    //e2 supports ___ auto-completion will only show e3 as an option
+    getRightVariables(allVariables: AstNodeDescription[], _context: CompletionContext): AstNodeDescription[]{
+        let rightVariables: AstNodeDescription[] = [];
         if(isSupport(_context.node)){
             if(_context.node.supporter.ref !== undefined){
                 let supporter_kind = _context.node.supporter.ref.kind;
                 let allowable_types = this.typeMap.get(supporter_kind);
-                console.log("allowable types for: " + supporter_kind  + ":");
-                allowable_types?.forEach((t)=> {
-                    console.log("\t" + t);
-                });
+
                 allVariables.forEach((variable)=>{
                     if(isVariable(variable.node)){
-                        let potential_kind = variable.node.kind;
-                        console.log("\tsupporter kind: " +supporter_kind + "\tsupportee name: " + variable.node.name);
-                        
-                        if(allowable_types?.includes(potential_kind)){
-                            console.log("pushing " + variable.node.name  +  " to supporteevariables");
-                            supporteeVariables.push(variable);
+                        if(allowable_types?.includes(variable.node.kind)){
+                            rightVariables.push(variable);
                         } 
                     }
                 });
             }
         }
         
-        return supporteeVariables;
+        return rightVariables;
     }
 
-    getSupporterVariables(allVariables: AstNodeDescription[], _context: CompletionContext): AstNodeDescription[]{
-        let supporterVariables: AstNodeDescription[] = [];
+    //only gives variables which can actually support other variables for autocompletion 
+    //ex. if your current document only defines evidence 'e1', strategy 'e2', 
+    //the autocompletion will only show evidence e1 as an autocompletion for the left support element
+    getLeftVariables(allVariables: AstNodeDescription[], _context: CompletionContext): AstNodeDescription[]{
+        let leftVariables: AstNodeDescription[] = [];
 
         allVariables.forEach((variable) =>{
             if(isVariable(variable.node)){
-                let variable_kind = variable.node.$type;
+                let variable_kind = variable.node.kind;
                 let allowable_types = this.typeMap.get(variable_kind);
-
+                
                 allVariables.forEach((possible_variable)=>{
                     if(isVariable(possible_variable.node)){
                         if(allowable_types?.includes(possible_variable.node.kind)){
-                            supporterVariables.push(possible_variable);
+                            leftVariables.push(variable);
                         }
                     }
                 });
             }
         });
-
-        return supporterVariables;
+        return leftVariables;
     }
-
-    stringer( arr: AstNodeDescription[]): string{
-        let p = "[ ";
-        arr.forEach((a) =>{
-            if(isVariable(a.node)){
-                p+=a.node.name;
-                p+=", ";
-            }
-        });
-        p+=" ]";
-        return p;
-    }
-    isPossibleSupporter1( potential_kind: string, _context: CompletionContext): boolean{
-        let supportee_kinds: string[] | undefined = this.typeMap.get(potential_kind);
-        let isPossibleSupporter: boolean = false;
-        console.log("\tsupporter kind: " + potential_kind +  "\tsupportee kind: " + supportee_kinds?.toString());
-        supportee_kinds?.forEach((supportee_kind) =>{
-            if(this.contextContains(supportee_kind, _context)){
-                console.log("\tcontext contains " + supportee_kind + "For " + potential_kind)
-                isPossibleSupporter = true;
-            }
-        });
-        return isPossibleSupporter;
-    }
-    contextContains(variableType:string, _context: CompletionContext): boolean{
-        let returnValue = false;
-        _context.document.references.forEach((cross_ref) =>{
-            console.log("\tchecking references: " + _context.node?.$type);
-            let ref = cross_ref.ref;
-            if(isVariable(ref)){
-                console.log("\tchecking cross ref: " + ref.name + "for " + variableType);
-                if(ref.kind === variableType){
-                    console.log("\tbariable Type: "  + variableType + "\tref name: " + ref.name + "\t ref kind: " + ref.kind);
-                    returnValue = true;
-                }
-            }
-        });
-        return returnValue;
-    }
-
 }
