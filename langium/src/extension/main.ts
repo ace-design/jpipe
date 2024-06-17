8e7f581646e42c4f18d53a8a06f2aaef95c5dc79
braunc8 <chloecbraun@gmail.com>
2024-06-17 14:40:15 -0400

    added ability to only execute command on a justification name if the diagram has an ending

--- langium/src/extension/main.ts
+++ langium/src/extension/main.ts
@@ -6,57 +6,7 @@ import { LanguageClient, TransportKind } from 'vscode-languageclient/node.js';
 
 let client: LanguageClient;
 
-//helper function which checks if the cursor is at a certain class type
-function cursorAt(class_type: string): boolean{//split into 2 functions to get if it is at the class, then one to set the vscode commands
-    const editor = window.activeTextEditor;
-    let cursor_at_class = false;
-    
-    if(editor){
-        const document = editor.document;
-        let word_range = document.getWordRangeAtPosition(editor.selection.active);
-        
-        if(word_range){
-            let class_correct = isClassCorrect(word_range, document, class_type);
-            let diagram_starts = diagramStarts(word_range, document);
-            //potentially add a check to see if the function ends (later version)
 
-            if(class_correct && diagram_starts){
-                cursor_at_class = true;
-            } 
-        }
-    }
-
-    return cursor_at_class;
-}
-
-//helper function to determine if the class type of a selected word is correct
-function isClassCorrect(word_range: Range, document: vscode.TextDocument, class_type: string): boolean{
-    let class_position = word_range.start.translate(0, -1);
-    let class_range = document.getWordRangeAtPosition(class_position);
-    let class_name = document.getText(class_range);
-
-
-    return (class_name === class_type);
-}
-
-//helper function for the class type verification process
-function diagramStarts(word_range: Range, document: vscode.TextDocument): boolean{
-    let function_starts: boolean;
-
-    let new_end = word_range.end.translate(0,2);
-    let search_range = new Range(word_range.end, new_end);
-    let search_text = document.getText(search_range);
-    
-    if(search_text.includes('{')){
-        function_starts = true;
-    }else{
-        function_starts = false;
-    }
-
-    return function_starts;
-}
-
-//function diagramEnds(word_range: Range, document: vscode.TextDocument): boolean{}
 
 // This function is called when the extension is activated.
 export function activate(context: vscode.ExtensionContext): void {
@@ -65,7 +15,6 @@ export function activate(context: vscode.ExtensionContext): void {
     window.onDidChangeTextEditorSelection(()=>{
         commands.executeCommand('setContext','jpipe.atJustification', cursorAt("justification"));        
     });
-    
 }
 
 // This function is called when the extension is deactivated.
@@ -106,4 +55,99 @@ function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
     // Start the client. This will also launch the server
     client.start();
     return client;
+}
+
+//helper function which checks if the cursor is at a certain class type
+function cursorAt(class_type: string): boolean{//split into 2 functions to get if it is at the class, then one to set the vscode commands
+    const editor = window.activeTextEditor;
+    let cursor_at_class = false;
+    
+    if(editor){
+        const document = editor.document;
+        let word_range = document.getWordRangeAtPosition(editor.selection.active);
+        
+        if(word_range){
+            let class_correct = isClassCorrect(word_range, document, class_type);
+            let diagram_starts = diagramStarts(word_range, document);
+            let diagram_ends = diagramEnds(word_range, document);
+
+            if(class_correct && diagram_starts && diagram_ends){
+                cursor_at_class = true;
+            } 
+        }
+    }
+
+    return cursor_at_class;
+}
+
+//helper function to get the word that the cursor is currently on
+function getCursorWord(): string{
+    const editor = window.activeTextEditor;
+    let cursor_word = "";
+    
+    if(editor){
+        const document = editor.document;
+        let word_range = document.getWordRangeAtPosition(editor.selection.active);
+        cursor_word = document.getText(word_range);
+    }
+
+    return cursor_word;
+}
+
+//helper function to determine if the class type of a selected word is correct
+function isClassCorrect(word_range: Range, document: vscode.TextDocument, class_type: string): boolean{
+    let class_name = getCursorWord();
+    let class_name_correct = class_name !== class_type;
+
+    let class_position = word_range.start.translate(0, -1);
+    let class_range = document.getWordRangeAtPosition(class_position);
+    let class_type_correct = document.getText(class_range) === class_type;
+    
+    return (class_name_correct && class_type_correct);
+}
+
+//helper function for the class type verification process
+function diagramStarts(word_range: Range, document: vscode.TextDocument): boolean{
+    let diagram_starts: boolean;
+
+    let new_end = word_range.end.translate(0,2);
+    let search_range = new Range(word_range.end, new_end);
+    let search_text = document.getText(search_range);
+    
+    if(search_text.includes('{')){
+        diagram_starts = true;
+    }else{
+        diagram_starts = false;
+    }
+
+    return diagram_starts;
+}
+
+//helper function to determine if the diagram ends
+function diagramEnds(word_range: Range, document: vscode.TextDocument): boolean{
+    let diagram_ends: boolean;
+
+    let pos_start = word_range.end;
+    let offset_end = document.offsetAt(pos_start)+1;
+    let pos_end = document.positionAt(offset_end);
+
+    let range = new Range(pos_start, pos_end);
+    let range_text = document.getText(range);
+
+    while(range_text && !range_text.includes('}')){
+        pos_start = range.end;
+        offset_end = document.offsetAt(pos_start)+1;
+        pos_end = document.positionAt(offset_end);
+    
+        range = new Range(pos_start, pos_end);
+        range_text = document.getText(range);
+    }
+
+    if(!range_text){
+        diagram_ends = false;
+    }else{
+        diagram_ends = true;
+    }
+
+    return diagram_ends;
 }
