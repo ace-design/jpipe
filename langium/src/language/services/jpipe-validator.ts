1ab8d723fe0a5e46d4dcd1b229a0ed15cd103455
braunc8 <chloecbraun@gmail.com>
2024-07-23 11:47:42 -0400

    fixed issue with whole document errors for support verification ONLY

--- langium/src/language/services/jpipe-validator.ts
+++ langium/src/language/services/jpipe-validator.ts
@@ -1,5 +1,5 @@
-import { type ValidationAcceptor, type ValidationChecks } from 'langium';
-import { isSupport, Support, type JpipeAstType, type Model} from '../generated/ast.js';
+import { Reference, type ValidationAcceptor, type ValidationChecks } from 'langium';
+import { isSupport, Support, Variable, type JpipeAstType, type Model} from '../generated/ast.js';
 import type { JpipeServices } from '../jpipe-module.js';
 import { hasSupports } from './jpipe-completion-provider.js';
 
@@ -25,7 +25,8 @@ export class JpipeValidator {
     }
 
     public testChecks(model: Model, accept: ValidationAcceptor): void{
-        this.checkSupportingStatements(model, accept);
+        this.checkVariables(model, accept);
+
     }
 
     //Checks that variables are defined
@@ -38,28 +39,62 @@ export class JpipeValidator {
                     }
                 });
             }
-
         });
     }
 
     //helper function to test if variables are defined
     private checkSupport(support: Support, accept: ValidationAcceptor): void{
-        if(this.hasError(support)){
-            let errorStatement = this.getErrorStatement(support);
-            accept("error", errorStatement, {node: support});
+        try{
+            let error = this.getErrorType(support.left, support.right);
+            if(error !== ErrorType.NoError){
+                let errorStatement = this.getErrorStatement(error);
+                accept("error", errorStatement.call(this, support), {node: support});
+            }
+        }catch(error: any){
+            accept("error", "Unknown issue with phrase", {node: support});
         }
     }
 
     //helper function to determine if there is an error in a support statement
-    private hasError(support: Support): boolean{
-        let left = support.left.ref;
-        let right = support.right.ref;
+    private getErrorType(left: Reference<Variable>, right: Reference<Variable>): ErrorType{
+        let errorType: ErrorType;
 
-        return left === undefined || right === undefined;
+        if(left.error !== undefined || right.error !== undefined){
+            errorType = ErrorType.LinkingError;
+        }else if(left.ref === undefined || right.ref === undefined){
+            errorType = ErrorType.ReferenceError;
+        }else{
+            errorType = ErrorType.NoError;
+        }
+
+        return errorType;
     }
 
     //helper function to determine the necessary error statement
-    private getErrorStatement(support: Support): string{
+    private getErrorStatement(errorType: ErrorType): (support: Support) => string{
+        let errorFunction: (support: Support) => string;
+
+        let errors = new Map<ErrorType, (support: Support) => string>([
+            [ErrorType.LinkingError, this.getLinkingError],
+            [ErrorType.ReferenceError, this.getReferenceError]
+        ]);  
+        
+        let returnFunction = errors.get(errorType);
+        
+        if(returnFunction){
+            errorFunction = returnFunction;
+        }else{
+            errorFunction =  () => "";
+        }
+        
+        return errorFunction;
+    }
+
+    private getLinkingError =  (support: Support): string => {
+        return "";
+    };
+    
+    private getReferenceError =  (support: Support): string => {
         let errorStatement: string
 
         let left = support.left.ref;
@@ -74,18 +109,17 @@ export class JpipeValidator {
         }
 
         return errorStatement;
-    }
+    };
 
     //checks if support statements follow proper typing convention
     private checkSupportingStatements(model: Model, accept: ValidationAcceptor): void{
-        
         model.entries.forEach( (entry) =>{
             if(hasSupports(entry)){
                 entry.body.supports.forEach( (support) =>{
                     if(isSupport(support)){
                         if(support.left.ref !== undefined && support.right.ref !==undefined){
-                            let leftKind = support.left.ref?.kind;
-                            let rightKind = support.right.ref?.kind;
+                            let leftKind = support.left.ref.kind;
+                            let rightKind = support.right.ref.kind;
                             let possibleRights: string[] | undefined = possible_supports.get(leftKind);
                                 
                             if(rightKind !== undefined){
@@ -105,6 +139,13 @@ export class JpipeValidator {
         });
     }
 }
+
+enum ErrorType{
+    NoError,
+    LinkingError,
+    ReferenceError
+}
+
 
 export var possible_supports = new Map<string,string[]>([
     ['evidence', ['strategy', '@support']],
