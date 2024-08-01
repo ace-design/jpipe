86ad54413d1fc40c1385ece54eef23ea44c67127
braunc8 <chloecbraun@gmail.com>
2024-08-01 10:25:52 -0400

    added strict scope provider which only adds loaded files

--- langium/src/language/services/jpipe-scope-provider.ts
+++ langium/src/language/services/jpipe-scope-provider.ts
@@ -8,94 +8,114 @@ export class JpipeScopeProvider extends DefaultScopeProvider{
         super(services)
         this.langiumDocuments = () => services.shared.workspace.LangiumDocuments;
     }
+
+    //gets the global scope for a given reference
     protected override getGlobalScope(referenceType: string, _context: ReferenceInfo): Scope {
-        let included_uris: Set<string> = new Set<string>();
+        let included_URIs: Set<URI> = new Set<URI>();
 
-        let load_statements = this.findLoadStatements(_context.container);
+        let current_URI = getCurrentURI(_context.container);
 
-        load_statements.forEach(load_statement =>{
-            let uri = URI.file(load_statement.name);
-            included_uris.add(uri.toString());
-        });
-
-        return this.globalScopeCache.get(referenceType, () => new MapScope(this.indexManager.allElements(referenceType, included_uris)));
-    }
-
-    //load statement type is preserved to implement loading of specific classes in the future
-    private findLoadStatements(node: AstNode): Load[]{
-        let load_statements: Load[] = [];
-        let model = getModelNode(node);
-        
-        model.loads.forEach(load_statement =>{
-            console.log(load_statement.name);
-            load_statements.push(load_statement);
-        });
-        
-        return load_statements;
-    }
-
-    //use complicated recursion to get all import statements referenced by the document
-    private getImports(document_imports: Set<URI>, all_imports: Set<URI>, doc_provider: LangiumDocuments): Set<URI>{
-        document_imports.forEach((doc_import) =>{
-            let load_statements = this.getLoadStatements(doc_import, doc_provider);
-
-            if(load_statements.length !== 0){
-                let new_document_uris = this.getURIs(load_statements);
-                this.getImports(new_document_uris, all_imports);
-            }
-        });
-    }
-//use complicated recursion to get all import statements referenced by the document
-private getImports(current_URI: URI, all_imports: Set<URI>, doc_provider: LangiumDocuments): Set<URI>{
-    if(all_imports.has(current_URI)){
-        return new Set<URI>();
-    }else{
-        all_imports.add(current_URI);
-
-        this.setImports(current_URI, doc_provider);
-
-
-        let load_URIs = this.uri_map.get(current_URI);
-    
-        if(load_URIs){
-            load_URIs.forEach(load_URI =>{
-                this.getImports(load_URI, all_imports, doc_provider);
-            });
+        if(current_URI){
+            included_URIs = this.getImports(current_URI, new Set<URI>(), this.langiumDocuments());
         }
-        return all_imports;
+        
+
+        return this.globalScopeCache.get(referenceType, () => new MapScope(this.indexManager.allElements(referenceType, toString(included_URIs))));
     }
-}
 
-
-private setImports(current_URI: URI, doc_provider: LangiumDocuments): void{
-
-    let load_URIs = getURIs(this.getNode(current_URI));
-
-    this.uri_map.set(current_URI, load_URIs);
-
-    load_URIs.forEach(load_URI =>{
-        this.setImports(load_URI, doc_provider);
-    });
-}
-
-
-private getNode(doc_import: URI): AstNode{
-    let model = this.indexManager.allElements("Class", new Set<string>([doc_import.toString()]));
-    let head = model.head();
-
-    if(head){
-        if(head.node){
-            return head.node;
+    //gets all imports from the current document recursively
+    private getImports(current_URI: URI, all_imports: Set<URI>, doc_provider: LangiumDocuments): Set<URI>{
+        if(all_imports.has(current_URI)){
+            return new Set<URI>();
         }else{
-            throw new Error("Node not found");
+            let load_URIs: Set<URI> | undefined;
+            let node = this.getNode(current_URI);            
+            
+            all_imports.add(current_URI);        
+            
+            if(node){
+                load_URIs = getURIs(node);
+            }
+        
+            if(load_URIs){
+                load_URIs.forEach(load_URI =>{
+                    this.getImports(load_URI, all_imports, doc_provider);
+                });
+            }
+            return all_imports;
         }
-    }else{
-        throw new Error("Node not found");
+    }
+
+    //gets any node from a uri
+    private getNode(doc_import: URI): AstNode | undefined{
+        let node: AstNode | undefined;
+        let model = this.indexManager.allElements(undefined, new Set<string>([doc_import.toString()]));
+        let head = model.head();
+
+        if(head){
+            if(head.node){
+                node = head.node;
+            }
+        }
+
+        return node;
     }
 }
+
+//turns a set of URIs into a set of strings
+function toString(URIs: Set<URI>): Set<string>{
+    let strings = new Set<string>();
+
+    URIs.forEach(uri =>{
+        strings.add(uri.toString());
+    });
+
+    return strings;
 }
 
-function getModelNode(node: AstNode): Model{
+//gets the current uri from a given node
+function getCurrentURI(node: AstNode): URI | undefined{
+    let current_URI: URI | undefined;
+    let model = getModelNode(node);
+    
+    if(model){
+        if(model.$document){
+            current_URI = model.$document.uri;
+        }
+    }
+
+    return current_URI;
+}
+
+//gets all URIs within a certain document from a node
+function getURIs(node: AstNode): Set<URI>{
+    let links = new Set<URI>;
+
+    let load_statements = findLoadStatements(node);
+
+    load_statements.forEach(load_statement =>{
+        links.add(URI.file(load_statement.name));
+    });
+
+    return links;
+}
+
+//finds all load statements in a document from a node
+function findLoadStatements(node: AstNode): Set<Load>{
+    let load_statements = new Set<Load>();
+    let model = getModelNode(node);
+
+    if(model){
+        model.loads.forEach(load_statement =>{
+            load_statements.add(load_statement);
+        });
+    }
+
+    return load_statements;
+}
+
+//gets the model node of a given node
+function getModelNode(node: AstNode): Model | undefined{
     let container = node.$container;
     while(container !== undefined && !isModel(container)){
         container = container.$container;
@@ -104,41 +124,6 @@ function getModelNode(node: AstNode): Model{
     if(isModel(container)){
         return container;
     }else{
-        throw new Error("Node has no Model");
+        return undefined;
     }
-}
-
-type LoadStatement = {
-    link: string
-}
-
-function isModelNode(node: AstNode | undefined): node is Model{
-    let is_model_node: boolean;
-
-    if(node !== undefined){
-        if(node.$type === 'Model'){
-            is_model_node = true;
-        }else{
-            is_model_node = false;
-        }
-    }else{
-        is_model_node = false;
-    }
-
-    return is_model_node;
-}
-function isClassNode(node: AstNode | undefined): node is Class{
-    let is_class_node: boolean;
-
-    if(node !== undefined){
-        if(node.$type === 'Class' || node.$type === 'Composition' || node.$type === 'JustificationPattern'){
-            is_class_node = true;
-        }else{
-            is_class_node = false;
-        }
-    }else{
-        is_class_node = false;
-    }
-
-    return is_class_node;
 }
