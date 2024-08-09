8676835cc13bba3c4f92c0ad89ce4437d338bfec
braunc8 <chloecbraun@gmail.com>
2024-08-09 14:37:19 -0400

    created potential generic validator, typed

--- langium/src/language/services/validation/main-validation.ts
+++ langium/src/language/services/validation/main-validation.ts
@@ -1,15 +1,19 @@
-import { AstNode, ValidationAcceptor, ValidationChecks } from 'langium';
+import { AstNode, CstNode, LangiumDocument, ValidationAcceptor, ValidationCheck, ValidationChecks } from 'langium';
 import type { JpipeServices } from '../../jpipe-module.js';
-import { JpipeAstType } from '../../generated/ast.js';
+import { JpipeAstType, Variable } from '../../generated/ast.js';
 import { PatternValidator, JustificationVariableValidator, SupportValidator, DeclarationValidator } from './validators/index.js';
 
 export class JpipeValidationRegistrar{
-    private validators: Array<Validator<any>>;
-    
+    private validators: Array<Validator<AstNode>>;
+    private checks: ValidationChecks<JpipeAstType>;
     constructor(services: JpipeServices){
         const registry = services.validation.ValidationRegistry;
+        
+        this.checks = {
 
-        this.validators = new Array<Validator<any>>(
+        }
+
+        this.validators = new Array<Validator<AstNode>>(
             new SupportValidator(),
             new JustificationVariableValidator(),
             new PatternValidator(),
@@ -20,6 +24,21 @@ export class JpipeValidationRegistrar{
             registry.register(validator.checks)
         })
     }
+}
+
+type AstNodeKey<K extends keyof JpipeAstType, T extends JpipeAstType[K]> = {[K in keyof T]: T[K] extends AstNode ? T[K]: never}
+
+export abstract class Validator1<K extends keyof JpipeAstType, T extends JpipeAstType[K], V extends AstNodeKey<K, T>>{
+    private readonly checks: ValidationChecks<JpipeAstType>;
+    private readonly check: ValidationCheck<V> = this.validate;
+
+    constructor(key: K){
+        this.checks = {
+            [key]: this.check
+        }
+    }
+
+    abstract validate(model: V, accept: ValidationAcceptor): void;
 }
 
 //when creating a validator, implement this interface
