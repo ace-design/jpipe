ee6b78a0207bcf3a69810675b11a3de8d540a874
Sébastien Mosser <sebastien.mosser@gmail.com>
2025-02-19 12:56:50 +0100

    exporting to SVG now maintain IDs

--- compiler/src/main/java/ca/mcscert/jpipe/visitors/exporters/GraphVizExporter.java
+++ compiler/src/main/java/ca/mcscert/jpipe/visitors/exporters/GraphVizExporter.java
@@ -14,6 +14,7 @@ import ca.mcscert.jpipe.model.elements.Pattern;
 import ca.mcscert.jpipe.model.elements.Strategy;
 import ca.mcscert.jpipe.model.elements.SubConclusion;
 import ca.mcscert.jpipe.visitors.ModelVisitor;
+import guru.nidi.graphviz.attribute.Attributes;
 import guru.nidi.graphviz.attribute.Color;
 import guru.nidi.graphviz.attribute.Label;
 import guru.nidi.graphviz.attribute.Rank;
@@ -26,6 +27,8 @@ import guru.nidi.graphviz.model.MutableNode;
  * Visit a jPipe model to produce a GraphViz Mutable Graph (used for image rendering).
  */
 public class GraphVizExporter extends ModelVisitor<MutableGraph> {
+
+    private String context = "";
 
     /**
      * Creates a Mutable Graph exporter visitor. Instantiating the accumulator as a properly
@@ -46,25 +49,29 @@ public class GraphVizExporter extends ModelVisitor<MutableGraph> {
 
     @Override
     public void visit(Justification j) {
+        this.context = j.getName();
         // Setting justification name
         this.accumulator.graphAttrs().add(Label.markdown(j.getName()));
         for (JustificationElement je : j.contents()) {
             je.accept(this);
         }
+        this.context = "";
     }
 
     @Override
     public void visit(Pattern p) {
+        this.context = p.getName();
         String label = "&lt;&lt;pattern&gt;&gt;&nbsp;" + p.getName();
         this.accumulator.graphAttrs().add(Label.html(label));
         for (JustificationElement je : p.contents()) {
             je.accept(this);
         }
+        this.context = "";
     }
 
     @Override
     public void visit(Conclusion c) {
-        MutableNode n = mutNode(c.getIdentifier())
+        MutableNode n = asMutableNode(c)
                 .add(Label.markdown(c.getLabel()))
                 .add(Shape.RECT)
                 .add(Style.combine(Style.FILLED, Style.ROUNDED))
@@ -79,7 +86,7 @@ public class GraphVizExporter extends ModelVisitor<MutableGraph> {
 
     @Override
     public void visit(Evidence e) {
-        MutableNode n = mutNode(e.getIdentifier())
+        MutableNode n = asMutableNode(e)
                 .add(Label.markdown(e.getLabel()))
                 .add(Shape.NOTE)
                 .add(Color.LIGHTSKYBLUE2.fill())
@@ -90,7 +97,7 @@ public class GraphVizExporter extends ModelVisitor<MutableGraph> {
 
     @Override
     public void visit(AbstractSupport as) {
-        MutableNode n = mutNode(as.getIdentifier())
+        MutableNode n = asMutableNode(as)
                 .add(Label.html("<i>" + as.getLabel() + "</i>"))
                 .add(Shape.RECT)
                 .add(Style.DOTTED)
@@ -100,7 +107,7 @@ public class GraphVizExporter extends ModelVisitor<MutableGraph> {
 
     @Override
     public void visit(Strategy s) {
-        MutableNode n = mutNode(s.getIdentifier())
+        MutableNode n = asMutableNode(s)
                 .add(Label.markdown(s.getLabel()))
                 .add(Shape.PARALLELOGRAM)
                 .add(Style.FILLED)
@@ -114,7 +121,7 @@ public class GraphVizExporter extends ModelVisitor<MutableGraph> {
 
     @Override
     public void visit(SubConclusion sc) {
-        MutableNode n = mutNode(sc.getIdentifier())
+        MutableNode n = asMutableNode(sc)
                 .add(Label.markdown(sc.getLabel()))
                 .add(Shape.RECT)
                 .add(Color.DODGERBLUE)
@@ -123,6 +130,15 @@ public class GraphVizExporter extends ModelVisitor<MutableGraph> {
         if (sc.getStrategy() != null) {
             this.accumulator.add(node(sc.getStrategy().getIdentifier()).link(n));
         }
+    }
+
+    private MutableNode asMutableNode(JustificationElement e) {
+        return mutNode(e.getIdentifier())
+                .add(Attributes.attr("id", buildIdentifier(e)));
+    }
+
+    private String buildIdentifier(JustificationElement e) {
+        return String.format("%s:%s", this.context, e.getIdentifier());
     }
 
 }
